6d1a823bb6c48d06b07dc83ba1541ea186041fe4
Chris Junker <elevatemedfit@gmail.com>
2019-08-16 18:13:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT title FROM projects ORDER BY ASC;"
+  "SELECT title FROM projects ORDER BY DESC;"
 end
 # SELECT Orders.OrderID, Customers.CustomerName
 # FROM Orders
