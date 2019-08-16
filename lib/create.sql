63da50e12ce4aded1883f2b7a9651cec73c454d8
Chris Junker <elevatemedfit@gmail.com>
2019-08-16 17:01:50 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY,title TEXT, catagory INTEGER,funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY,title TEXT, field TEXT,funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
 CREATE TABLE users (id INTEGER PRIMARY KEY);
 CREATE TABLE pledges (id INTEGER PRIMARY KEY);
