f452efbd352ba987985e03790b44427c54b078aa
Chris Junker <elevatemedfit@gmail.com>
2019-08-16 19:53:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
     FROM projects
     JOIN pledges ON projects.id = pledges.project_id
-    GROUP BY projects.title 
+    GROUP BY projects.title
     HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   JOIN pledges ON users.id = pledges.user_id
   GROUP BY users.name
-  ORDER BY SUM(pledges.amount);"  
+  ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
